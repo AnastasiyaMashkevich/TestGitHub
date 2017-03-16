8c3547b630bf9fb57b85954e7d59e2a8d62d7053
Anastasiya Mashkevich <nastya.mashkevich@gmail.com>
2017-03-16 15:29:10 +0300

    add new project

---
+++ src/main/java/epam/com/Square.java
@@ -0,0 +1,11 @@
+package epam.com;
+
+/**
+ * Created by anastasiya_mashkevic on 3/16/17.
+ */
+public class Square {
+    public int findSquare (int a, int b)
+    {
+        return a*b;
+    }
+}
---
+++ src/test/java/epam/com/SquareTest.java
@@ -0,0 +1,26 @@
+package epam.com;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+/**
+ * Created by anastasiya_mashkevic on 3/16/17.
+ */
+public class SquareTest {
+    Square square =new Square();
+
+    @DataProvider (name = "DateProviderForSquare")
+    public Object [] [] paramForSquareTest (){
+        return new Object [] []{
+                {2, 3, 6},
+                {2, 4, 8}
+        };
+    }
+
+    @Test (dataProvider = "DateProviderForSquare")
+    public void squareTest (int a, int b, int res) {
+        Assert.assertEquals(square.findSquare(a, b),res);
+    }
+
+}
